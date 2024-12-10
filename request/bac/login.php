b4e51165c5ec25414898f04704254dd4e778f7ef
MariecrisC <95411054+MariecrisC@users.noreply.github.com>
2024-12-10 13:57:43 +0800

    Create login
    Create BAC login

---
+++ request/bac/login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BAC Log In</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+</head>
+
+<body class="bg-gray-50">
+<div class="sm:mx-auto sm:w-full sm:max-w-sm text-center mb-6 mt-10">
+    <h2 class="text-4xl font-extrabold text-gray-900">Automated Purchase Request System</h2>
+    <p class="mt-2 text-sm text-gray-600">Submit your purchase requests with ease and efficiency.</p>
+  </div>
+
+
+<div class="flex min-h-full flex-col justify-center py-12 px-6 lg:px-8 bg-white shadow-lg rounded-lg mx-4 sm:mx-auto sm:w-full sm:max-w-lg">
+
+  <div class="sm:mx-auto sm:w-full sm:max-w-sm text-center">
+    <h2 class="text-3xl font-extrabold text-gray-900">BAC Log In</h2>
+    <p class="mt-2 text-sm text-gray-600">Please enter your credentials to access the dashboard.</p>
+  </div>
+
+</body>
+</html>
